0d99442fdd1a9f70e08cd70027ea46137fa90033
John Bradshaw <124829389+Red-Rockwell@users.noreply.github.com>
2026-03-01 23:45:05 +0000

    Add files via upload
    Manually added files.

---
+++ BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by John Bradshaw on 12/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
